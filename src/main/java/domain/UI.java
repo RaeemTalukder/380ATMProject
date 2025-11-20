7ac774f77421e0137c5653c8d7b3ab5db872dd6d
Raeem Talukder <raeem2003@gmail.com>
2025-11-20 14:53:05 -0800

    my changes

--- src/main/java/domain/UI.java
+++ src/main/java/domain/UI.java
@@ -1,58 +1,74 @@
 package domain;
+import java.util.Scanner;
 import com.zaxxer.hikari.HikariDataSource;
 import database.DatabaseManager;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
 
-
-
-public class UI{
+public class UI {
+    Enum <> bills;
+    EnumMap <> coins;
     private static final String JDBC_URL =
             "jdbc:postgresql://aws-1-us-east-1.pooler.supabase.com:5432/postgres?user=postgres.kjcrwhinbakxmxnzjswk&password=380ATMProject";
-    public void guestui(long cardnumber, LocalDateTime timestamp, int pin, String name, boolean ourBranch, double balance, boolean isAdmin){
+    public void guestui(long cardNumber, int pin, String name, boolean ourBranch, double balance, boolean isAdmin){
         Scanner sc = new Scanner(System.in);
-        Account ac = new Account(cardnumber, pin, name, ourBranch, balance, isAdmin);
         HikariDataSource ds = new HikariDataSource();
         DatabaseManager dm = new DatabaseManager(ds);
+        Account ac = new Account(cardNumber, pin, name, ourBranch, balance, isAdmin);
         Transaction Transaction;
-        ATM atm = new ATM(dm);
+        ATM atm = new ATM();
         System.out.println("Welcome, Enter a number to continue");
         System.out.print("1) Deposit");
         System.out.print("2) Withdraw");
         System.out.print("3) View Balance");
         System.out.print("4) Transaction History");
-        System.out.print("5) Exit");
+        System.out.print("5) Logout");
         int command = sc.nextInt();
         switch(command){
             case 1:
-                System.out.println("Enter Desired amount to deposit (Enter 0 to return to main menu):");
-                int damount= sc.nextInt();
+                System.out.println("Amount of ones to deposit?");
+                int dones = sc.nextInt();
+                System.out.println("Amount of Fives to deposit?");
+                int dfives = sc.nextInt();
+                System.out.println("Amount of Tens to deposit?");
+                int dtens = sc.nextInt();
+                System.out.println("Amount of twenties to deposit?");
+                int dtwenties = sc.nextInt();
+                System.out.println("Amount of fifties to deposit?");
+                int dfifties = sc.nextInt();
+                System.out.println("Amount of hundred to deposit?");
+                int dhundred = sc.nextInt();
 
-                if(damount != 0) {
-                    Transaction = new Transaction(domain.Transaction.TransactionType.DEPOSIT, damount, cardnumber, timestamp);
-                } else{
-                    guestui(cardnumber, timestamp, pin, name, ourBranch, balance, isAdmin);
-                }
+                atm.deposit();
+
+
+
             case 2:
-                System.out.println("Enter Desired amount to withdraw (Enter 0 to return to main menu):");
-                int wamount= sc.nextInt();
-                if(wamount != 0){
-                Transaction t = new Transaction(domain.Transaction.TransactionType.WITHDRAWAL, wamount, cardnumber, timestamp);}
-                else { guestui(cardnumber,  timestamp, pin, name, ourBranch, balance, isAdmin);
-                }
+                System.out.println("Amount of ones to withdraw?");
+                int wones = sc.nextInt();
+                System.out.println("Amount of Fives? to withdraw");
+                int wfives = sc.nextInt();
+                System.out.println("Amount of Tens? to withdraw");
+                int wtens = sc.nextInt();
+                System.out.println("Amount of twenties? to withdraw");
+                int wtwenties = sc.nextInt();
+                System.out.println("Amount of fifties? to withdraw");
+                int wfifties = sc.nextInt();
+                System.out.println("Amount of hundred ? to withdraw");
+                int whundred = sc.nextInt();
+
             case 3:
                 double b = ac.getBalance();
                 System.out.println("Current Balance:" + b);
-                guestui(cardnumber, timestamp, pin, name, ourBranch, balance, isAdmin);
+
             case 4:
                 System.out.println("Transaction History: " );
                 ac.printTransactions();
-                guestui(cardnumber, timestamp, pin, name, ourBranch, balance, isAdmin);
+                guestui(cardNumber, pin, name, ourBranch, balance, isAdmin);
             case 5:
-                guestui(cardnumber, timestamp, pin, name, ourBranch, balance, isAdmin);
+                LoginManager LM = new LoginManager(atm, dm);
+
         }
     }
-    public void adminui(){
+    public void adminui() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter command");
         System.out.println("1)Open ATM");
@@ -64,24 +80,21 @@ public class UI{
         ATM atm = new ATM(dm);
         Account ac;
         int command = sc.nextInt();
-        switch(command){
+        switch (command) {
             case 1:
 
             case 2:
             case 3:
 
         }
+    }
+    public static HikariDataSource getDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(JDBC_URL);
+        ds.setAutoCommit(false);
+        return ds;
+
 
     }
-
-    }
-
-public static HikariDataSource getDataSource() {
-    HikariDataSource ds = new HikariDataSource();
-    ds.setJdbcUrl(JDBC_URL);
-    ds.setAutoCommit(false);
-    return ds;
-
-
-
 }
+
